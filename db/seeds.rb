8e07bfbb048936e2e5e3a3c3b673ce1fc2824cd1
Umair Ejaz <umairejaz.ch@gmail.com>
2020-09-06 19:11:19 +0400

    seeds uncommented

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,21 @@
 require "csv"
 
-# CSV.foreach(Rails.root.join("movies.csv"), {:headers => true}) do |csv|
-#
-#   title, description, year, director, actor, filming_locations, filming_country = csv[0], csv[1], csv[2], csv[3],
-#       csv[4], csv[5], csv[6]
-#
-#   movie = Movie.find_by_title title
-#   if movie.blank?
-#     Movie.create(title: title, year: year, director: director, filming_country: filming_country,
-#                  filming_locations: filming_locations)
-#   else
-#     actor_obj = Actor.find_or_create_by(name: actor)
-#     movie.actors << actor_obj
-#     movie.save!
-#   end
-#
-# end
+CSV.foreach(Rails.root.join("movies.csv"), {:headers => true}) do |csv|
+
+  title, description, year, director, actor, filming_locations, filming_country = csv[0], csv[1], csv[2], csv[3],
+      csv[4], csv[5], csv[6]
+
+  movie = Movie.find_by_title title
+  if movie.blank?
+    Movie.create(title: title, year: year, director: director, filming_country: filming_country,
+                 filming_locations: filming_locations)
+  else
+    actor_obj = Actor.find_or_create_by(name: actor)
+    movie.actors << actor_obj
+    movie.save!
+  end
+
+end
 
 CSV.foreach(Rails.root.join("reviews.csv"), {:headers => true}) do |csv|
 
